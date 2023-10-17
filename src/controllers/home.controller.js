8d00fe425f15e86fcba5f9cdc7bffa976bf019cb
Alexander Toapanta <ktoapanta@fibramax.ec>
2023-10-17 11:36:40 -0500

    Update home link

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,5 +1,5 @@
 export const homePage = (req, res) => {
   return res.send(
-    '<a href="https://github.com/atoapanta/API-CRUD-NODEJS" target="_blank">¡Welcome to APIREST, development by @atoapanta!</a>'
+    '<a href="https://github.com/atoapanta/apirest-jwt" target="_blank">¡Welcome to APIREST, development by @atoapanta!</a>'
   );
 };
